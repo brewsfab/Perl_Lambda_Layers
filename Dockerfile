aa73acfdc59a9b2b8e1a3a87b8bb84ba2a8de987
William-Fabrice BROU <will@wni.com>
2020-07-16 18:21:53 +0900

    Full installation & Layers creation

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,9 @@
 FROM centos:latest
 
-RUN dnf -y install dnf install glibc-langpack-en
-RUN yum -y update && yum clean all
-RUN yum -y groupinstall "Development Tools"
-
+ENV MOUNTVOL /outfiles
+RUN mkdir -p $MOUNTVOL
+COPY fullinstall.sh /tmp
 WORKDIR /tmp
-
-RUN curl https://downloads.activestate.com/ActivePerl/releases/5.24.3.2404/ActivePerl-5.24.3.2404-x86_64-linux-glibc-2.15-404865.tar.gz -O
-
-RUN tar -xf ActivePerl-5.24.3.2404-x86_64-linux-glibc-2.15-404865.tar.gz
-
-WORKDIR ActivePerl-5.24.3.2404-x86_64-linux-glibc-2.15-404865
+RUN chmod 755 fullinstall.sh && ./fullinstall.sh
 
 ENTRYPOINT ["/bin/bash"]
---
+++ fullinstall.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+dnf -y install dnf install glibc-langpack-en
+yum -y update && yum clean all
+yum -y groupinstall "Development Tools"
+
+cd /tmp
+
+curl -LO https://downloads.activestate.com/ActivePerl/releases/5.24.3.2404/ActivePerl-5.24.3.2404-x86_64-linux-glibc-2.15-404865.tar.gz
+
+tar -xf ActivePerl-5.24.3.2404-x86_64-linux-glibc-2.15-404865.tar.gz
+
+cd ActivePerl-5.24.3.2404-x86_64-linux-glibc-2.15-404865
+
+./install.sh --prefix /opt/perl --license-accepted --no-install-html --no-komodo
+
+/opt/perl/bin/cpan -y install Paws
+/opt/perl/bin/cpan -y install Data::Printer
+
+cd /opt/perl
+rm -rf html man
+
+cd site
+rm -rf html man
+
+#Make sure that the folder exists before creating the link
+mkdir -p /opt/bin/
+
+ln -s /opt/perl/bin/perl /opt/bin/perl
+
+cd /opt
+
+curl -LO https://gist.githubusercontent.com/brewsfab/78bfa60bcc579af10eb51b3b454a75a4/raw/46aae5d119960bf07b88405d37d2b8742da80196/bootstrap
+curl -LO https://gist.githubusercontent.com/brewsfab/78bfa60bcc579af10eb51b3b454a75a4/raw/46aae5d119960bf07b88405d37d2b8742da80196/perl_runtime.pl.tpl
+
+chmod 755 bootstrap perl_runtime.pl.tpl
+
+zip -r $MOUNTVOL/perl.zip perl bin/perl -x perl/site/lib/Paws/\* -x perl/site/lib/Paws.pm
+zip -r $MOUNTVOL/paws.zip perl/site/lib/Paws perl/site/lib/Paws.pm
+zip -r $MOUNTVOL/perl-runtime.zip bootstrap perl_runtime.pl.tpl
+
